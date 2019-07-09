62c122bef872cb48732ea01ab57d45d98ffb77d8
FabianB14 <32110808+FabianB14@users.noreply.github.com>
2019-07-09 11:52:34 -0700

    This weather features complete

--- server.js
+++ server.js
@@ -11,12 +11,21 @@ app.use(cors());
 //API Routes
 app.get('/location', (request, response)=>{
   console.log(request.query);
-  // try{
-  const locationData = searchToLatLng('Seattle');
-  response.send(locationData)
-  // }catch(e){
-  //   response.status(500).send('status 500: Sorry I am not working')
-  // }
+  try{
+    const locationData = searchToLatLng('Seattle');
+    response.send(locationData)
+  }catch(e){
+    response.status(500).send('status 500: Sorry I am not working')
+  }
+})
+app.get('/weather', (request, response)=>{
+  console.log(request.query);
+  try{
+    const weatherData = searchWeather();
+    response.send(weatherData)
+  }catch(e){
+    response.status(500).send('status 500: Sorry I am not working')
+  }
 })
 
 app.use('*',(request,response)=>{
@@ -26,6 +35,20 @@ function Location(geoData){
   this.formatted_query = geoData.results[0].formatted_address;
   this.latitude = geoData.results[0].geometry.location.lat;
   this.longitude = geoData.results[0].geometry.location.lng;
+}
+function Weather(weatherData){
+  this.forecast = weatherData.summary;
+  let weatherTime = weatherData.time;
+  this.time = new Date(weatherTime)
+}
+function searchWeather(weatherQuery){
+  const weatherArr = [];
+  const darkSkyData = require('./data/darksky.json');
+  darkSkyData.daily.data.forEach(day =>{
+    weatherArr.push(new Weather(day));
+  })
+  weatherArr.search_query = weatherQuery;
+  return weatherArr;
 }
 
 function searchToLatLng(query){
